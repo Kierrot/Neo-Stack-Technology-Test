5b863966b161e2a4b9f30b188676f82211a4f6a4
Kierrot <code030366@yandex.ru>
2024-03-25 19:57:44 +0700

    Update MainWindow.xaml.cs

--- Gen/MainWindow.xaml.cs
+++ Gen/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿
-using Eco.ViewModel.Runtime;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Data.SqlTypes;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Gen
